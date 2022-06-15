f6390af488e3a8c20a7e491fe5bf3ffd33f3e88f
panda-lee-58 <hongshan840508@gmail.com>
2022-06-15 13:14:09 +0900

    トップページ：ポップアップの表示

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
 /* javascriptのコードを記載 */
+
+$(document).ready(function() {
+    console.log( "ready!" );
+
+    $('#btn-lead').on('click', function() {
+        $('#popup').fadeIn(300);
+    });
+
+    $('#popup-bg').on('click', function() {
+        $('#popup').hide();
+    });
+});
