3171de99446ad9797203cb532f2bfd64e75cda52
panda-lee-58 <hongshan840508@gmail.com>
2022-06-15 12:12:28 +0900

    スタイルの調整

--- index.php
+++ index.php
@@ -15,16 +15,16 @@ include_once('./head.php');
             </form>
             <div id="btn-lead" class="btn-lead">お見積り、仮お申し込みまでの流れ</div>
             <div class="lead">
-                <p class="txt-lead">
+                <p class="txt txt-has-star">
                     インターネットの通信環境によっては都道府県を選択後、自動で市区町村が表示されない場合がございます。その場合、お手数ですが都道府県を選択後、少し時間をおいてから市区町村を選択して下さい。
                 </p>
-                <p class="txt-lead">
+                <p class="txt txt-has-star">
                     旧居もしくはご新居が4階以上のマンション)の場合、別途料金となります。
                 </p>
-                <p class="txt-lead">
+                <p class="txt txt-has-star">
                     ご家財や間取りにより、窓吊り作業等の特殊作業が発生した際は別途料金となります。
                 </p>
-                <p class="txt-lead">
+                <p class="txt txt-has-star">
                     現住所、転居先が離島の場合はオペレーター対応となりますので、<br>下部フリーダイヤルボタン、お問い合わせボタンより、お電話、メールにてお問い合わせ下さい。<br>(受付時間AM8:00～PM5:00　水曜日定休日)までお電話ください。
                 </p>
             </div>
