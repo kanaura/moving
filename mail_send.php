c9cc5acdac38269c0c8ab96699de0a1b493e9fc6
panda-lee-58 <hongshan840508@gmail.com>
2022-08-01 17:58:13 +0900

    修正

--- mail_send.php
+++ mail_send.php
@@ -16,49 +16,93 @@ function str_date($date)
     return date('Y年m月d日 ', strtotime($date));
 }
 
+$message_common = "[お客様情報]\r\n";
+$message_common .= "引越の種類：" . $_POST['final_moving_type'] . "\r\n";
+$message_common .= "お名前：" . $_POST['final_name'] . "\r\n";
+$message_common .= "ふりがな：" . $_POST['final_gana'] . "\r\n";
+$message_common .= "電話番号：" . $_POST['final_tel'] . "\r\n";
+$message_common .= "メールアドレス：" . $_POST['final_email'] . "\r\n";
+$message_common .= "現住所：\r\n";
+$message_common .= $_POST['final_post_code_01'] . "\r\n";
+$message_common .= $_POST['final_addr_01_01'] . " " . $_POST['final_addr_01_02'] . "\r\n";
+$message_common .= $_POST['final_addr_01_type'] . "\r\n";
+$message_common .= "転居先：\r\n";
+$message_common .= $_POST['final_post_code_02'] . "\r\n";
+$message_common .= $_POST['final_addr_02_01'] . " " . $_POST['final_addr_02_02'] . "\r\n";
+$message_common .= $_POST['final_addr_02_type'] . "\r\n";
+$message_common .= "ご連絡方法：" . $_POST['final_contact_method'] . "\r\n\r\n";
+
+$message_common .= "[お見積り内容]\r\n";
+$message_common .= "お引越要望日：" . str_date($_SESSION['date']) . " " . (($_SESSION['time'] == "am") ? "午前" : "午後") . "\r\n";
+$message_common .= "基本料金：" . $_SESSION['price_good'] . "円\r\n";
+$message_common .= "オプション：" . $_SESSION['price_option'] . "円\r\n";
+$message_common .= "消費税：" . $_SESSION['price_tax'] . "円\r\n";
+$message_common .= "合計金額：" . $_SESSION['price_total'] . "円\r\n\r\n";
+
+$message_common .= "[入力内容]\r\n";
+$message_common .= "【家財】\r\n";
+for ($i = 0; $i < count($_SESSION['goods_ttl']); $i++) {
+    if ($_SESSION['goods_cnt'][$i] != 0) {
+        $message_common .= $_SESSION['goods_ttl'][$i] . "：" . $_SESSION['goods_cnt'][$i] . "台\r\n";
+    }
+}
+
+$message_common .= "\r\n";
+$message_common .= "【オプション】\r\n";
+for ($i = 0; $i < count($_SESSION['options_ttl']); $i++) {
+    if ($_SESSION['options_cnt'][$i] != 0) {
+        $message_common .= $_SESSION['options_ttl'][$i] . "：" . $_SESSION['options_cnt'][$i] . "台\r\n";
+    }
+}
+$message_common .= "\r\n";
+
 $mailto  = "aketchi.07@gmail.com";
 $option  = "From:<info@hacovice.com>";
 
 $title   = "お引越しの仮お申し込み入りました";
 $message = "非対面カンタンお見積りシステムより仮お申し込みを受け付けました。\r\n\r\n";
-$message .= "[お客様情報]\r\n";
-$message .= "引越の種類：" . $_POST['final_moving_type'] . "\r\n";
-$message .= "お名前：" . $_POST['final_name'] . "\r\n";
-$message .= "ふりがな：" . $_POST['final_gana'] . "\r\n";
-$message .= "電話番号：" . $_POST['final_tel'] . "\r\n";
-$message .= "メールアドレス：" . $_POST['final_email'] . "\r\n";
-$message .= "現住所：\r\n";
-$message .= $_POST['final_post_code_01'] . "\r\n";
-$message .= $_POST['final_addr_01_01'] . " " . $_POST['final_addr_01_02'] . "\r\n";
-$message .= $_POST['final_addr_01_type'] . "\r\n";
-$message .= "転居先：\r\n";
-$message .= $_POST['final_post_code_02'] . "\r\n";
-$message .= $_POST['final_addr_02_01'] . " " . $_POST['final_addr_02_02'] . "\r\n";
-$message .= $_POST['final_addr_02_type'] . "\r\n";
-$message .= "ご連絡方法：" . $_POST['final_contact_method'] . "\r\n\r\n";
+$message = $message . $message_common;
 
-$message .= "[お見積り内容]\r\n";
-$message .= "お引越要望日：" . str_date($_SESSION['date']) . " " . (($_SESSION['time'] == "am") ? "午前" : "午後") . "\r\n";
-$message .= "基本料金：" . $_SESSION['price_good'] . "円\r\n";
-$message .= "オプション：" . $_SESSION['price_option'] . "円\r\n";
-$message .= "消費税：" . $_SESSION['price_tax'] . "円\r\n";
-$message .= "合計金額：" . $_SESSION['price_total'] . "円\r\n\r\n";
-
-$message .= "[入力内容]\r\n";
-$message .= "【家財】\r\n";
-for ($i = 0; $i < count($_SESSION['goods_ttl']); $i++) {
-    if ($_SESSION['goods_cnt'][$i] != 0) {
-        $message .= $_SESSION['goods_ttl'][$i] . "：" . $_SESSION['goods_cnt'][$i] . "台\r\n";
-    }
+if (!mb_send_mail($mailto, $title, $message, $option)) {
+    // echo "送信失敗";
+    header("Location: thanks.php?res=true");
+    exit();
 }
 
-$message .= "\r\n";
-$message .= "【オプション】\r\n";
-for ($i = 0; $i < count($_SESSION['options_ttl']); $i++) {
-    if ($_SESSION['options_cnt'][$i] != 0) {
-        $message .= $_SESSION['options_ttl'][$i] . "：" . $_SESSION['options_cnt'][$i] . "台\r\n";
-    }
-}
-$message .= "\r\n";
+$mailto  = $_POST['final_email'];
 
-mb_send_mail($mailto, $title, $message, $option);
+$title   = "お引越しの仮お申し込みありがとうございます【株式会社ハコビス】";
+$message = "※このメールは自動返信メールです。\r\n";
+$message .= "この度は、株式会社ハコビスに\r\n";
+$message .= "お引越しの仮お申し込みをいただき\r\n";
+$message .= "誠にありがとうございます。\r\n\r\n";
+$message .= "お申し込みいただい内容は次の通りです。\r\n";
+$message .= "担当者より、あらためてご連絡させていただきますので、\r\n";
+$message .= "今しばらくお待ちください。\r\n";
+$message = $message . $message_common;
+
+$message .= "\r\n";
+$message .= "───────────────\r\n";
+$message .= "株式会社 ハコビス\r\n";
+$message .= "本社\r\n";
+$message .= "〒791-1102\r\n";
+$message .= "愛媛県松山市来住町1178-5\r\n";
+$message .= "TEL 089-909-7720\r\n";
+$message .= "FAX 089-993-5626\r\n\r\n";
+
+$message .= "広島オフィス\r\n";
+$message .= "〒730-0016\r\n";
+$message .= "広島県広島市中区幟町13-15\r\n";
+$message .= "新広島ビルディング 2階\r\n";
+$message .= "TEL 082-512-2257\r\n\r\n";
+$message .= "https://www.hacovice.com/company/\r\n";
+
+if (mb_send_mail($mailto, $title, $message, $option)) {
+    // echo "送信成功";
+    header("Location: thanks.php?res=true");
+    exit();
+} else {
+    // echo "送信失敗";
+    header("Location: thanks.php?res=false");
+    exit();
+}
---
+++ thanks.php
@@ -0,0 +1,74 @@
+<?php
+
+// $page_ttl = '住所の選択 | 株式会社ハコビズ';
+$page_ttl = '住所の選択 | 株式会社ハコビス';
+
+include_once('./head.php');
+?>
+
+<body class="home">
+    <?php include_once('./header.php'); ?>
+
+    <div class="wrap">
+        <div class="content">
+            <div class="main-figure">
+                <img src="./assets/img/main.png" alt="" />
+            </div>
+            <div class="lead">
+                <p class="txt" style="font-size: 36px; font-weight: bold; margin: 0 0 60px; text-align: center;">
+                    本当にありがとうございました！
+                </p>
+            </div>
+            <a class="btn-default" href="./address.php">ホームページへ</a>
+        </div>
+    </div>
+
+    <div id="popup" class="popup">
+        <div id="popup-bg" class="popup-bg"></div>
+        <div class="popup-body">
+            <p class="popup-body-ttl">お見積り、仮お申し込みまでのカンタン4ステップ</p>
+            <div class="popup-block">
+                <p class="popup-block-ttl">1.「現住所」と「転居先」を選択。</p>
+                <p class="popup-block-txt">
+                    現住所と転居先の「都道府県」「市町村」を選択。
+                </p>
+            </div>
+            <div class="popup-block">
+                <p class="popup-block-ttl">2.「お運びする家財」と「オプション」を選択。</p>
+                <p class="popup-block-txt mb-20">
+                    お運びする家財をリスト内から選んで個数を入力。
+                    <span>リスト内に家財がない場合は「フリー入力欄」に家財名・サイズ（縦・横・高さ）を入力。</span>
+                </p>
+                <p class="popup-block-txt">
+                    オプションをリストから選択。
+                    <span>リスト以外のオプションも承っておりますので、リストにない場合は<br>「ご要望欄」にご記入ください。<br>追ってお見積りさせていただきます。</span>
+                </p>
+            </div>
+            <div class="popup-block">
+                <p class="popup-block-ttl">3.「引越希望日」を選択。</p>
+                <p class="popup-block-txt">
+                    カレンダー内のご希望日・時間帯を選択。
+                </p>
+            </div>
+            <div class="popup-block">
+                <p class="popup-block-ttl">
+                    4.「お見積り金額」の確認▶︎<br>
+                    　　　　　　　「仮お申し込み」。
+                </p>
+                <p class="popup-block-txt">
+                    ご入力いただいた情報からお見積り金額の<br>
+                    概算が表示されます。<br>
+                    ご確認後、「お客様情報」を入力して<br>
+                    「仮お申し込み」ボタンを押して完了です。。<br>
+                    追って、弊社より折り返しご連絡させていただきます。
+                </p>
+            </div>
+            <div class="popup-figure">
+                <img src="./assets/img/home/popup.svg" alt="" />
+            </div>
+        </div>
+    </div>
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
