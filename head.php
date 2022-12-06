b4cc18de1f10f4736a395e53e7c0429f26be046d
panda-lee-58 <hongshan840508@gmail.com>
2022-12-06 17:46:15 +0900

    index.phpの追加

---
+++ head.php
@@ -0,0 +1,18 @@
+<!-- HTMLコード -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Script-Type" content="text/javascript" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+    <meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?=$page_ttl;?></title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <!-- javascript -->
+    <script src="./libs/jquery/jquery-3.6.0.min.js"></script>
+</head>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$page_ttl = '株式会社ハコビス';
+
+include_once('./head.php');
+?>
+
+<body class="home">
+    <div class="wrap">
+    </div>
+
+    <div id="popup" class="popup">
+    </div>
+
+    <script src="./assets/js/script.js"></script>
+</body>
+</html>
